1aac5140b3814f64afbebf289512f0fe178ea278
tawmairie <taw.mairie.aixenprovence@gmail.com>
2022-11-25 10:59:46 +0100

    Add file to project

---
+++ android/src/main/java/com/tawmairie/reactnativesmsandroidsend/RNSmsAndroidPackage.java
@@ -0,0 +1,27 @@
+package com.tawmairie.reactnativesmsandroidsend;
+
+import android.app.Activity;
+
+import java.util.*;
+
+import com.facebook.react.ReactPackage;
+import com.facebook.react.bridge.JavaScriptModule;
+import com.facebook.react.bridge.NativeModule;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.uimanager.ViewManager;
+
+public class RNSmsAndroidPackage implements ReactPackage {
+    private RNSmsAndroidModule mModuleInstance;
+
+    @Override
+    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+      mModuleInstance = new RNSmsAndroidModule(reactContext);
+      return Arrays.<NativeModule>asList(mModuleInstance);
+    }
+    
+    @Override
+    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+        return Collections.emptyList();
+    }
+
+}
